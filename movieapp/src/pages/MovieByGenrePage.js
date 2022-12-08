a7801a3c5737e2a04cd92ccc453384f818b484f1
TanUve <tania.velasco.carvajal@gmail.com>
2022-12-08 18:50:18 +0100

    add star rating and pagination

--- movieapp/src/pages/MovieByGenrePage.js
+++ movieapp/src/pages/MovieByGenrePage.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import Rating from '@mui/material/Rating';
 import './movieByGenderPage.css';
+
 
 
 function MovieByGenrePage() {
 
-    const urlGenre = `https://api.themoviedb.org/3/discover/movie?api_key=fb1999e69926d1387eb44c3abee6e7c5&language=es-ES&with_genres=28&primary_release_year=2022`
+    const urlGenre = `https://api.themoviedb.org/3/discover/movie?api_key=fb1999e69926d1387eb44c3abee6e7c5&language=en-EN&with_genres={idgenre}&primary_release_year=2022`
     const urlImg = `https://image.tmdb.org/t/p/original`;
-    const urlGenreId = `https://api.themoviedb.org/3/genre/movie/list?api_key=90c2c57ed9eabcec0ae2b8ebe7b81547&language=es-ES`
+    const urlGenreId = `https://api.themoviedb.org/3/genre/movie/list?api_key=90c2c57ed9eabcec0ae2b8ebe7b81547&language=en-EN`
     const [movies, setMovies] = useState([]);
-    const [genres, setGenres] = useState();
+    const [genres, setGenres] = useState([]);
+    const [page, setPage] = useState(0);
+    const maxMovie = movies.length / 10
+
 
     //Obtenemos todas las películas
     useEffect(() => {
@@ -33,23 +38,38 @@ function MovieByGenrePage() {
         getGenreId();
     }, [])
 
+
+
     return (
         <>
             <header className='movieListHeader'>
-                <h1 className='genre'>| Género <img src='https://cdn-icons-png.flaticon.com/512/8893/8893034.png' width="35px"/></h1>
-                <button className='returnButton'><img src='https://cdn-icons-png.flaticon.com/512/8893/8893000.png' width="16px"/>   Volver</button>
+                <h1 className='genre'>|Science Fiction <img src='https://cdn-icons-png.flaticon.com/512/8893/8893034.png' width="19spx" /></h1>
+                <button className='returnButton'><img src='https://cdn-icons-png.flaticon.com/512/8893/8893000.png' width="16px" />   Volver</button>
             </header>
             <div className='container'>
 
-                {movies.map((movie, genre) =>
+                {movies.slice(page * 12, (page + 1) * 12).map((movie) =>
                     <div className='movies'>
-                        <img src={urlImg + movie.poster_path} width="280px" />
+                        <img class="poster" src={urlImg + movie.poster_path} width="220px" />
                         <p className='title'>{movie.title}</p>
+                        <p className='rating'>{movie.vote_average / 2} / 5</p>
+                        <Rating className="ratingComp" name="half-rating-read" value={movie.vote_average / 2} precision={0.05} max={5} readOnly />
                     </div>
                 )}
 
-
             </div>
+            <div id='btns'>
+                <button id="prev" onClick={() => {
+                    if (page > 0) {
+                        setPage(page - 1)
+                    }
+                }}> Anterior</button>
+                <p id='page'>{page+1}</p>
+                <button id='next' onClick={() => {
+                    if (page < (maxMovie) - 1) { setPage(page + 1) }
+                }}>Siguiente</button>
+            </div >
+
         </>
     )
 }
