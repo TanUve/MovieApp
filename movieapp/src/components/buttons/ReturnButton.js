cba2b70f5d2b5beca1acaa4b1914436d002d8929
TanUve <tania.velasco.carvajal@gmail.com>
2022-12-10 19:16:03 +0100

    createdd component: return button

---
+++ movieapp/src/components/buttons/ReturnButton.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../styles/movieByGenrePage.css';
+
+function ReturnButton() {
+
+
+    return (
+        <>
+        
+            <button className='returnButton'><img src='https://cdn-icons-png.flaticon.com/512/8893/8893000.png' width="16px" />   Volver</button>
+        
+        </>
+    )
+};
+
+
+export default ReturnButton;
