327157ca276567badd1c847179bf3d4d0ee6e448
MPPS26 <mp.pesoto@gmail.com>
2022-12-11 18:37:49 +0100

    nuevo cambio

--- movieapp/src/App.js
+++ movieapp/src/App.js
@@ -2,14 +2,14 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import './App.css';
 import Carousel from './Carousel';
+/*import "react-image-gallery/styles/css/image-gallery.css";
+import ImageGallery from 'react-image-gallery'*/
 
 function App() {
-
-  /*const [movieData, setMovie] = useState ([]);*/
   
   return (
     <div className="App">
-              <Carousel
+        <Carousel
         />
     </div>
   );
