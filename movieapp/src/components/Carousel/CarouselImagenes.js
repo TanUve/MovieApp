ba74561be18202e0ccac5b9d8279c5d96ff90585
TanUve <112825834+TanUve@users.noreply.github.com>
2022-12-20 19:07:16 +0100

    Update CarouselImagenes.js

--- movieapp/src/components/Carousel/CarouselImagenes.js
+++ movieapp/src/components/Carousel/CarouselImagenes.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import '../../styles/ImageCarousel.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import React, { Component } from "react";
+import React from "react";
 import Slider from "react-slick";
 import { useNavigate } from 'react-router-dom';
 
